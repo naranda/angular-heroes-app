b1d91f936a0b713301db5db8b0ebc69bb72ce716
Nicolas Aranda <nicolas.aranda@gmail.com>
2022-10-25 12:54:31 -0300

    fin seccion 13 del curso

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-buscar',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe | undefined;
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+  }
+
+  buscando() {
+    this.heroesService.getSugerencias(this.termino.trim())
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+
+    if (!event.option.value) {
+      console.log("No hay valor");
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+    const heroe = event.option.value;
+    console.log(heroe);
+
+    this.termino = heroe.superhero;
+
+    this.heroesService.getHeroePorId(heroe.id!).subscribe(heroe => this.heroeSeleccionado = heroe);
+
   }
 
 }
